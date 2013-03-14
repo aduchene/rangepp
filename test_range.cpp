278ea3b7f5e0aabc953b3882bb68bdbecac9e38d
ared38 <ared38@gmail.com>
2013-03-14 12:34:40 -0500

    Fixed header include

--- test_range.cpp
+++ test_range.cpp
@@ -1,10 +1,12 @@
-#include "range_naive.hpp"
+#include "range.hpp"
 
 #include <iostream>
+#include <functional>
 using namespace std;
 
 int main(){
-    for(int i : range(9)){
+    auto r=bind(range,9);
+    for(int i : r()){
         cout<<i<<endl;
     }
     return 0;
